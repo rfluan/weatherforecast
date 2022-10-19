d93a62fec888260a1d109bc7fa43436254b65653
rfluan <luanramosferr@outlook.com>
2022-10-19 11:45:22 -0300

    html feito

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,2 @@
 // ea0e322b3953e5e7f95acdc440ee2c56
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=ea0e322b3953e5e7f95acdc440ee2c56
+// https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=ea0e322b3953e5e7f95acdc440ee2c56
