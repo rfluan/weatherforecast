17c6536811e32480eea6d2941f67fa0de619164e
rfluan <luanramosferr@outlook.com>
2022-10-19 21:08:43 -0300

    add const

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,20 @@
-// ea0e322b3953e5e7f95acdc440ee2c56
-// https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=ea0e322b3953e5e7f95acdc440ee2c56
+// Variáveis e seleção de elementos
+const apiKey = "ea0e322b3953e5e7f95acdc440ee2c56";
+const apiContryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+//Funções
+const showWeatherData = (city) => {
+    console.log(city)
+}
+
+//Eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+        const city = cityInput.value;
+
+    showWeatherData(city);
+});
